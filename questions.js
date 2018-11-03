52930801975353af037e18749626927d32a6b6ed
Yoonsoo Park <ypark9@ncsu.edu>
2018-11-02 22:46:10 -0400

    initial commit comment

---
+++ questions.js
@@ -0,0 +1,427 @@
+'"use strict",';
+/**
+  * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+  * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+  */
+module.exports = {
+    "QUESTIONS_EN_US": [
+        //1
+        {
+            "What are the three main ingredients in traditional Mexican salsa?":
+                [
+                    "Tomato, Jalapeno Pepper, Cilantro",
+
+                    "Bell Peppers, Tomato, Cucumber",
+
+                    "Onion, Tomato, Paprika",
+
+                    "Tomato, Olive, Red Bell Pepper"
+                ]
+        },
+        {
+            "What is guacamole made from?":
+                [
+                    "Avocados",
+
+                    "Snap Peas",
+
+                    "Basil",
+
+                    "Granny Smith Apples",
+
+                ]
+        }
+        ,
+        {
+            "What kind of Greek bread is used in a gyro?":
+                [
+                    "Pita Bread ",
+                    "Polaki Bread ",
+                    "Wheat Germ Bread ",
+                    "Gyro Bread"
+                ]
+        }
+        ,
+        {
+            "What do you include with the chicken, to make Chicken Cordon Bleu?":
+                [
+                    "Ham And Cheese",
+                    "Apples And Bacon",
+
+                    "Truffles And Potatoes",
+
+                    "Mushrooms And Scallions"
+                ]
+        }
+        ,
+        {
+            "What is Vichyssoise?":
+                [
+                    "Cold French Soup",
+
+                    "Italian Ice Cream",
+                    
+                    "Spanish Rice",
+                    
+                    "A Type Of Chip Dip"
+                    
+                ]
+        }
+        ,
+        {
+            "The food pierogi comes from what country?":
+            [
+                "Poland",
+                
+                "China",
+            
+                "Italy",
+                
+                "Canada"
+                
+            ]
+ 
+        }
+        ,
+        {
+            "In what country did okonomiyaki originate?":
+                [
+                    "Japan",
+                    "Thailand",
+
+                    "Brazil",
+
+                    "South Africa"
+
+                ]
+        }
+        ,
+        {
+            "What is Coq au Vin?":
+                [
+                    "Chicken With Wine",
+
+                    "Eggs With Mushrooms",
+
+                    "Chicken With Brie",
+                    
+                    "Eggs With Brie"
+                    
+                    
+                ]
+        }
+        ,
+        {
+            "What is a main ingredient in ratatouille?":
+                [
+                    "Eggplant",
+
+                    "Milk",
+
+                    "Potatoes",
+                    
+                    "Okra"
+                    
+                ]
+        }
+        ,
+        {
+            "What dish from Spain consists of saffron rice, shrimp, clams, chicken, sausage and peas?":
+                [
+                    "Paella",
+
+                    "Sangria",
+                    
+                    "Gazpacho",
+                    
+                    "Arroz Con Pollo"
+                    
+                ]
+        }
+        ,
+        {
+            "What is the most commonly used oil in Chinese cooking?":
+                [
+                    "Groundnut Or Peanut Oil",
+
+                    "Sunflower Oil",
+
+                    "Olive Oil",
+                    
+                    "Corn Oil"
+                    
+                    
+                ]
+        }
+        ,
+        {
+            "Brown peas and bacon are traditionally cooked in which country for Christmas morning?":
+                [
+                    "Latvia",
+
+                    "Germany",
+
+                    "Poland",
+                    
+                    "Spain"
+                    
+                    
+                ]
+        }
+        ,
+        {
+            "In which country is a dry cod-fish traditionally eaten on Christmas morning?":
+                [
+                    "Portugal",
+                    "Spain ",
+                    "England ", 
+                    "Germany"
+                ]
+        }
+        ,
+        {
+            "Melba toast was invented at which London hotel?":
+                [
+                    "Savoy",
+                    "Grosvenor House ", 
+                    "Langham ",
+                    "Ritz"
+                ]
+        }
+        ,
+        {
+            "In Philippine cuisine, what is lechon?":
+                [
+                    "Roasted Pig",
+
+                    "Sauce Made From Pork Liver",
+                    
+                    "Sauteed Eggplant In Vinegar",
+                    
+                    "Sweet And Sour Steamed Garoupa Fish"
+                ]
+        }
+        ,
+        {
+            "The chef of which French emperor first prepared veal marengo?":
+                [
+                    "Napoleon The First",
+
+                    "Louis The Fourteenth",
+
+                    "Napoleon The Third",
+                    
+                    
+                    "Louis The Sixteenth"
+                ]
+        }
+        ,
+        {
+            "Donburi is a general term for what Japanese word?":
+                [
+                    "Bowl ",
+                    "Rice ",
+                    "Dog ",
+                    "Steak"
+                ]
+        }
+        ,
+        {
+            "Enoki is what type of Japanese vegetable?":
+                [
+                    "Mushroom",
+
+                    "Radish",
+
+                    "Carrot",
+                    
+                    "Turnip"
+                    
+                    
+                ]
+        }
+        ,
+        {
+            "Nikujaga is a combination of meat and what?":
+                [
+                    "Potatoes",
+
+                    "Bell Peppers",
+
+                    
+                    "Rice",
+                    
+                    "Noodles"
+                    
+                ]
+        }
+        ,
+        {
+            "A basic okonomiyaki is a pancake made of what?":
+                [
+                    "Yam And Cabbage",
+
+                    "Tuna",
+                    
+                    "Noodles",
+                    
+                    "Jelly"
+                    
+                ]
+        }
+        ,
+        {
+            "What strong-smelling Japanese breakfast food is made from soybeans?":
+                [
+                    "Natto",
+
+                    "Domburi",
+
+                    
+                    "Okonomiyaki",
+                    
+                    "Nikujaga"
+                    
+                ]
+        }
+        ,
+        {
+            "Which of the following is not a popular type of sushi?":
+                [
+
+                    "Udon",
+
+                    "Norimaki",
+
+                    
+                    "Gunkan",
+                    
+                    "Nigiri"
+                    
+                ]
+        }
+        ,
+        {
+            "What is the English word for the French seafood called anguille?":
+                [
+                    "Eel ",
+                    "Frog ",
+                    "Oyster ",
+                    "Scallop "
+                ]
+        }
+        ,
+        {
+            "Which culture is responsible for inventing hors d'oeuvres?":
+                [
+                    "Athenians",
+
+                    "French",
+
+                    "Aztecs",
+                    
+                    "Ancient Egyptians"
+                    
+                ]
+        }
+        ,
+        {
+            "What is the main staple carb in Japanese food?":
+                [
+                    "Wheat",
+
+                    "Rice",
+
+                    "Corn",
+
+                    "Barley"
+                    
+                ]
+        }
+        ,
+        {
+            "What is the Japanese dish hoshizakana?":
+                [
+                    "Fish",
+
+                    "Rice Casserole",
+                    
+                    "Sweet Cake",
+                    
+                    "Steamed Veggies"
+                    
+                ]
+        }
+        ,
+        {
+            "What are you served if you try the signature dish isfabada in northwestern Spain?":
+                [
+                    "Bean Stew",
+
+                    "Raw Fish",
+
+                    "Vinegar Wine",
+                    
+                    "Garlic Spiced Salad"
+                    
+                ]
+        }
+        ,
+        {
+            "The Calaveras de Azucar, or sugar skulls, are a candy served on what Mexican holiday?":
+                [
+                    "Day Of The Dead",
+
+                    "Cinco De Mayo",
+                    
+                    "Christmas",
+                    
+                    "Three Kings Day"
+                    
+                ]
+        }
+        ,
+        {
+            "What was the first year of Oktoberfest, the annual beer & food festival in Munich?":
+                [
+                    "1810",
+
+                    "1945",
+                    
+                    "1547",
+                    
+                    "1746"
+                    
+                ]
+        }
+        ,
+        {
+            "At a Japanese restaurant, if you order yakizakana what will arrive on your plate?":
+                [
+                    "Grilled Fish",
+
+                    "Chinese Noodles With Ginger",
+                    
+                    "Raw Fish",
+                    
+                    "Steamed Rice With Green Tea"
+                    
+                ]
+        }
+        ,
+        {
+            "Which of the following is an authentic Pocky (Japanese biscuit treat) style?":
+                [
+                    "Mens Pocky",
+
+                    "Pavlova",
+                    
+                    "Kiwiberry",
+                    
+                    "Starfruit"
+                    
+                ]
+        }
+
+    ]
+};
